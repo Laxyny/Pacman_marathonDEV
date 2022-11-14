60483425af5d13460b58085979210b74b36abbb8
Laxyny <kevgreg30@gmail.com>
2022-11-14 16:14:31 +0100

    changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from variables import *
 import math
 pygame.init()
 
-#Générer fenêtre du jeu
+# Générer fenêtre du jeu
 WIDTH = 1280
 HEIGHT = 720
 color = 'blue'
@@ -15,8 +15,9 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 running = True
 level = boards
 
-#Charger notre jeu
+# Charger notre jeu
 game = Game()
+
 
 def draw_board():
     num1 = ((HEIGHT - 50) // 32)
@@ -24,9 +25,11 @@ def draw_board():
     for i in range(len(level)):
         for j in range(len(level[i])):
             if level[i][j] == 1:
-                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+                pygame.draw.circle(
+                    screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
             if level[i][j] == 2:
-                pygame.draw.circle(screen, 'yellow', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+                pygame.draw.circle(
+                    screen, 'yellow', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
             if level[i][j] == 3:
                 pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
                                  (j * num2 + (0.5 * num2), i * num1 + num1), 3)
@@ -44,45 +47,49 @@ def draw_board():
                                 3 * PI / 2, 3)
             if level[i][j] == 8:
                 pygame.draw.arc(screen, color,
-                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 -
+                                                                 (0.4 * num1)), num2, num1], 3 * PI / 2,
                                 2 * PI, 3)
             if level[i][j] == 9:
-                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                pygame.draw.line(screen, 'red', (j * num2, i * num1 + (0.5 * num1)),
                                  (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
-            
-#Boucle exécutée tant que running = True
+
+            # Vérifier si le joueur souhaite aller a gauche ou a droite
+    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x < 1250 and (level[i][j] == (1 | 2) or screen.fill('black')):
+        game.player.move_right()
+    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0 and (level[i][j] == (1 | 2) or screen.fill('black')):
+        game.player.move_left()
+    elif game.pressed.get(pygame.K_UP) and game.player.rect.y > 0 and (level[i][j] == (1 | 2) or screen.fill('black')):
+        game.player.move_up()
+    elif game.pressed.get(pygame.K_DOWN) and game.player.rect.y < 620 and (level[i][j] == (1 | 2) or screen.fill('black')):
+        game.player.move_down()
+
+
+# Boucle exécutée tant que running = True
 while running:
 
-    #Appliquer arrière plan du jeu
+    # Appliquer arrière plan du jeu
     screen.fill('black')
     draw_board()
 
-    #Appliquer l'image du joueur
+    # Appliquer l'image du joueur
     screen.blit(game.player.image, game.player.rect)
 
-    #Vérifier si le joueur souhaite aller a gauche ou a droite
-    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x < 1250:
-        game.player.move_right()
-    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
-        game.player.move_left()
-    elif game.pressed.get(pygame.K_UP) and game.player.rect.y > 0:
-        game.player.move_up()
-    elif game.pressed.get(pygame.K_DOWN) and game.player.rect.y < 620:
-        game.player.move_down()
+    
 
     print(game.player.rect.y)
 
-    #Mettre à jour l'écran
+    # Mettre à jour l'écran
     pygame.display.flip()
-    
-    #Si le joueur ferme la fenêtre
+
+    # Si le joueur ferme la fenêtre
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
             print("Fermeture du jeu")
-        
-        #Détection si un joueur lache une touche du clavier
+
+        # Détection si un joueur lache une touche du clavier
         elif event.type == pygame.KEYDOWN:
             game.pressed[event.key] = True
         elif event.type == pygame.KEYUP:
