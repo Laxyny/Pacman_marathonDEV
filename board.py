1fa617df21daf3a6614dbb9934b9d43d8e609675
Laxyny <kevgreg30@gmail.com>
2022-11-16 12:04:57 +0100

    Changes

--- board.py
+++
@@ -1,5 +0,0 @@
-#Classe board
-class Board():
-    def __init__(self, game):
-        super().__init__()
-        
--- player.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-#Classe joueur
-class Player(pygame.sprite.Sprite):
-    def __init__(self, game):
-        super().__init__()
-        
